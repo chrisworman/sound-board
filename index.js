f38f946a98259d1bce325f2479fb70454045cc95
Chris Worman <chrisworman@gmail.com>
2023-11-18 10:28:57 -0800

    Files

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 class SoundBoardApp {
-    constructor() {
-        this.sprite = {
+	constructor() {
+		this.sprite = {
 			fucking_guy: [0, 1188],
 			what_have_you_done: [2000, 1762],
 			no_cookies: [4000, 3031],
 			bone_broth: [8000, 21500],
+			its_illegal: [31000, 2125],
+			gimmie_dat: [35500, 750],
 		};
 		this.sound = new Howl({
 			src: ["sound-board-sprite.mp3"],
 			sprite: this.sprite,
 		});
-        this.currentlyPlayingSpriteName = null;
-    }
+		this.currentlyPlayingSpriteName = null;
+	}
 
 	run() {
 		const buttonsElement = document.getElementById("buttons");
@@ -19,19 +21,19 @@ class SoundBoardApp {
 			const button = document.createElement("button");
 			button.innerText = spriteName.replace(/_/g, " ");
 			button.addEventListener("click", () => {
-                this.handleButtonClick(spriteName);
+				this.handleButtonClick(spriteName);
 			});
 			buttonsElement.appendChild(button);
 		}
 	}
 
-    handleButtonClick(spriteName) {
-        if (this.currentlyPlayingSpriteName) {
-            this.sound.stop();
-        }
-        this.sound.play(spriteName);
-        this.currentlyPlayingSpriteName = spriteName;
-    }
+	handleButtonClick(spriteName) {
+		if (this.currentlyPlayingSpriteName) {
+			this.sound.stop();
+		}
+		this.sound.play(spriteName);
+		this.currentlyPlayingSpriteName = spriteName;
+	}
 }
 
 const app = new SoundBoardApp();
