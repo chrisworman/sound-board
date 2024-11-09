661b441f16797b88bbbf20523834cfdabde49d5b
Chris Worman <chrisworman@gmail.com>
2024-11-09 09:26:10 -0800

    New sound; new buttons; playing state ui

--- index.js
+++ index.js
@@ -9,15 +9,24 @@ class SoundBoardApp {
 			gimmie_dat: [35500, 750],
 			no_table_questions: [36250, 2688],
 			'i_can\'t_know_tables': [38938, 2938],
+			'and I\'m fucking scared': [41876, 3281],
 		};
+		this.currentlyPlayingSpriteName = null;
+		document.getElementById('startButton').addEventListener('click', () => {
+			this.run();
+		});
+		this.buttons = new Map();
+	}
+
+	run() {
+		document.getElementById('startButton').remove();
 		this.sound = new Howl({
 			src: ["sound-board-sprite.mp3"],
 			sprite: this.sprite,
 		});
-		this.currentlyPlayingSpriteName = null;
-	}
-
-	run() {
+		this.sound.on('end', () => {
+			this.setButtonState(this.currentlyPlayingSpriteName, false);
+		});
 		const buttonsElement = document.getElementById("buttons");
 		for (let spriteName in this.sprite) {
 			const button = document.createElement("button");
@@ -25,6 +34,7 @@ class SoundBoardApp {
 			button.addEventListener("click", () => {
 				this.handleButtonClick(spriteName);
 			});
+			this.buttons.set(spriteName, button);
 			buttonsElement.appendChild(button);
 		}
 	}
@@ -32,11 +42,21 @@ class SoundBoardApp {
 	handleButtonClick(spriteName) {
 		if (this.currentlyPlayingSpriteName) {
 			this.sound.stop();
+			this.setButtonState(this.currentlyPlayingSpriteName, false);
 		}
 		this.sound.play(spriteName);
 		this.currentlyPlayingSpriteName = spriteName;
+		this.setButtonState(spriteName, true);
+	}
+
+	setButtonState(spriteName, isPlaying) {
+		const button = this.buttons.get(spriteName);
+		if (isPlaying) {
+			button.classList.add('playing');
+		} else {
+			button.classList.remove('playing');
+		}
 	}
 }
 
-const app = new SoundBoardApp();
-app.run();
+new SoundBoardApp();
